2e2232d3e5c23f8ab66d7c53f407892913b12acd
Grizzly <uibbs2@gmail.com>
2017-06-07 18:15:35 +0200

    Restarted from scratch of the structure

---
+++ Servizi.pm
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+# Riferimento per le librerie richieste da OpenMCE
+# Copyright (C) 2017 by Grizzly  http://www.g-sr.eu/
+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see .
+
+# This programme is CardWare: if you like it, please send me a pictured postcard
+# This programme is TeddyWare: if you *enjoyed* it, please send me a stuffed teddy bear (-:
+# Sell your items to:
+#     Grizzly aka Mirko Tuccitto c/o Undefined Illusion BBS
+#     12A, via Salvatore Monteforte - 96100 Siracusa ITALY
+
+##### 
+
+package Servizi;
+
+sub some_function {
+    # put sane code here
+    return 123;
+}
+
+1;
---
+++ openmce.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use LWP::UserAgent;
+use MIME::Base64;
+use Config::Abstract::Ini;
+
+	#TEMP
+	use Data::Dumper;
+
+use Servizi;
+
+print Servizi::some_function();  # prints 123
+
